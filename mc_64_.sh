313b7d4bf302d5c17db33dcd47cbe654848b5c00
mansoor <mansoorkodungallur@gmail.com>
2018-03-16 12:30:29 +0530

    c installation script

---
+++ mc_64_.sh
@@ -0,0 +1,9 @@
+# Installation Dependencies on 64-bit Linux for  xc8 compiler
+
+
+sudo apt-get install libc6:i386 libx11-6:i386 libxext6:i386 libstdc++6:i386 libexpat1:i386
+
+
+sudo chmod u+x x*
+
+sudo ./x*
